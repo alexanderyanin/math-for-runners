3e1dd9fdaed93680687e0151c0bdf31f3cfa1f9b
Alexander Ryanin <alexander@ryanin.ru>
2023-06-26 07:07:18 +1000

    formatting

--- heart_rate.py
+++ heart_rate.py
@@ -1,10 +1,12 @@
 print()
+normal_text = 'Нормальный ЧСС:'
+low_text = 'Нижний ЧСС:'
+high_text = 'Верхний ЧСС:'
 age = input('Укажите ваш возраст: ')
 normal = 220 - int(age)
 low = normal * 65 / 100
 high = normal * 85 / 100
 print()
-print('Нормальный ЧСС: ', normal, 'уд/мин', end='\n\n')
-print('Нижний ЧСС: ', low, 'уд/мин')
-print('Верхний ЧСС: ', high, 'уд/мин')
-
+print(f'{normal_text:20} {normal:10.2f} уд/мин', end='\n\n')
+print(f'{low_text:20} {low:10.2f} уд/мин')
+print(f'{high_text:20} {high:10.2f} уд/мин')
